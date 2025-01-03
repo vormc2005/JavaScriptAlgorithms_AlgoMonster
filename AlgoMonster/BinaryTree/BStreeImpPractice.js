5e46d6ee46ff505ad279ec2bf886d3b3c7ab3fb2
vormc2005 <voronov.dmitry@gmail.com>
2025-01-03 14:46:44 -0500

    added trees

--- AlgoMonster/BinaryTree/BStreeImpPractice.js
+++ AlgoMonster/BinaryTree/BStreeImpPractice.js
@@ -1,105 +1,183 @@
-class Node{
-    constructor(value){
+class Node {
+    constructor(value) {
+        this.value = value;
         this.left = null;
         this.right = null;
-        this.value = value;
     }
 }
 
-class BinarySearchTree{
-    constructor(){
+class BinarySearchTree {
+    constructor() {
         this.root = null;
     }
 
-    insert(value){
-        const newNode = new Node(value);
-
-        if(this.root === null) {
-            this.root = newNode;
-        }else{
-            let currentNode = this.root;
-            while(true){
-                if(currentNode.value > value){
-                    //left
-                    if(!currentNode.left){
-                        currentNode.left = newNode;
-                        return this;
-                    }
-                    currentNode = currentNode.left;
-
-                }else{
-                    //Right
-                    if(!currentNode.right){
-                        currentNode.right = newNode;
+    insert(value) {
+        let newNode = new Node(value);
+        if (this.root === null) {
+            this.root = newNode
+            return this
+        } else {
+            var current = this.root;
+            while (true) {
+                if (value === current.value) return undefined
+                if (value < current.value) {
+                    if (current.left === null) {
+                        current.left = newNode
                         return this
-                    }  
-                    currentNode = currentNode.right;
+                    } else {
+                        current = current.left
+                    }
+                } else if (value > current.value) {
+                    if (current.right === null) {
+                        current.right = newNode
+                        return this
+                    } else {
+                        current = current.right
+                    }
                 }
             }
         }
-
     }
 
-   breathFirstSearch(){
-        let currentNode = this.root;
-        let list = [];
-        let queue = [];
-        queue.push(currentNode);
-        while(queue.length > 0){
-            currentNode = queue.shift();
-            list.push(currentNode.value);
-            if(currentNode.left){
-                queue.push(currentNode.left)
-            }
-            if(currentNode.right){
-                queue.push(currentNode.right);
-            }
-        }
-        return list;
-   }
-
-    lookup(value){
-        if(this.root){
-            return false;
-        }
-        let currentNode = this.root;
-
-        while(currentNode){
-            if(currentNode.value > value){
-                //left
-                currentNode = currentNode.left;
-            }else if(currentNode.value < value){
-                //right
-                currentNode = currentNode.right
-            }else if(currentNode.value === value){
-                return currentNode
+    lookup(value) {
+        if (!this.root) return false
+        let current = this.root
+        while (current) {
+            if (value < current.value) {
+                current = current.left
+            } else if (value > current.value) {
+                current = current.right
+            } else if (value === current.value) {
+                return current
             }
         }
         return false
     }
+
+
+    bfs() {
+        let current = this.root
+        let list = []
+        let queue = []
+        queue.push(current)
+        while (queue.length > 0) {
+            current = queue.shift()
+            list.push(current.value)
+            if (current.left) queue.push(current.left)
+            if (current.right) queue.push(current.right)
+        }
+        return list
+    }
+
+    // dfs(root,target){    
+    //     if(root === null) return null
+    //     if(root.value === target) return root
+    //     const left = this.dfs(root.left, target)
+    //     if(left !== null) return left
+    //     const right = this.dfs(root.right, target)
+    //     return right
+    // }
+
+    doTheSearch(target) {
+        return this.dfs(this.root, target)
+    }
+
+    inOrderTraversal(root) {
+        if (root !== null) {
+            this.inOrderTraversal(root.left)
+            console.log(root.val)
+            this.inOrderTraversal(root.right)
+        }
+    }
+    traverse() {
+        return this.inOrderTraversal(this.root)
+    }
+  
+    
+
+    getTreeRoot() {
+        return this.root
+    }
+
 }
 
 
-const tree = new BinarySearchTree();
-tree.insert(9);
-tree.insert(4);
-tree.insert(6);
-tree.insert(20);
-tree.insert(170);
-tree.insert(15);
-tree.insert(1);
-// console.log(JSON.stringify(tree))
-// console.log(JSON.stringify(traverse(tree.root)));
 
-console.log(tree.breathFirstSearch())
+var tree = new BinarySearchTree();
+// tree.root = new Node(10);
+// tree.root.right = new Node(15);
+// tree.root.left = new Node(7);
+// tree.root.left.right = new Node(9);
 
-
-function traverse(node) {
-    const tree = { value: node.value };
-    tree.left = node.left === null ? null : traverse(node.left);
-    tree.right = node.right === null ? null : traverse(node.right);
-    return tree;
-  }
+// Add using insert method
+tree.insert(10)
+tree.insert(5)
+tree.insert(13)
+tree.insert(11)
+tree.insert(2)
+tree.insert(16)
+tree.insert(10)
+// console.log(tree.traverse())
 
 
-  
+
+// function inOrderTraversal(tree){
+//     let root = 
+//     if(root !== null) {
+//         this.inOrderTraversal(root.left)
+//         console.log(node.val)
+//         this.inOrderTraversal(root.right)
+//     }
+// }
+// console.log(tree.lookup(5))
+// console.log(tree.bfs())
+
+// console.log(tree.doTheSearch(11))
+// console.log(tree.inOrderTraversal())
+let root = tree.getTreeRoot()
+
+
+function dfs(node) {
+    if (node === null) return 0
+    return Math.max(dfs(node.left), dfs(node.right)) + 1
+}
+function treeMaxDepth() {
+    return tree.getTreeRoot() !== null ? dfs(tree.getTreeRoot()) - 1 : 0;
+}
+
+
+function treeHeight(tree){
+    if(tree === null)return 0
+    const leftHeight = treeHeight(tree.left)
+    const rightHeight = treeHeight(tree.right)
+    if(leftHeight === -1 || rightHeight === -1) return -1
+    if(Math.abs(leftHeight - rightHeight)>1) return -1
+    return Math.max(leftHeight, rightHeight)+1;
+}
+/**/
+function isBalanced(){
+    return treeHeight(root) !==-1
+}
+
+/*is subtree of another*/
+function subtreeofAnotherTree(root, subRoot){
+    if(!root) return false
+    return isSame(root, subRoot) || subtreeofAnotherTree(root.left, subRoot) || subtreeofAnotherTree(root.right, subRoot)
+}
+
+function isSame(tree1, tree2){
+    if(!tree1 && !tree2) return true
+    if(!tree || !tree) return false
+    return tree1.val === tree2.val && isSame(tree1.left, tree2.left) && isSame(tree1.right && tree2.right)
+}
+
+
+function invertTree(tree){
+    if(tree === null) return null
+   
+    return new Node(tree.val, invertTree(tree.right), invertTree(tree.left))
+}
+console.log(invertTree(root))
+
+console.log(isBalanced())
+console.log(treeMaxDepth())
